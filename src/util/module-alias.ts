3b7e0cd06c9fd3a6b2b9497ca99484ca6dbd2574
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-10 09:23:13 -0300

    add prettier

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -4,6 +4,6 @@ import moduleAlias from 'module-alias';
 const files = path.join(__dirname, '../..');
 
 moduleAlias.addAliases({
-    '@src': path.join(files, 'src'),
-    '@test': path.join(files, 'test')
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test'),
 });
